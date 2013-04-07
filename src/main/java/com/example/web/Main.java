b5f276a0bb803fdf124c8d667f5510f19b000db4
rory <rory@warthog.(none)>
2013-04-07 07:57:37 +0100

    Initial commit of hello world app

---
+++ src/main/java/com/example/web/Main.java
@@ -0,0 +1,41 @@
+package com.example.web;
+
+import java.io.File;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class Main implements spark.servlet.SparkApplication {
+
+    public Main() {
+        setupRoutes();
+    }
+
+    public void setupRoutes() {
+        Spark.get(new Route("/") {
+
+            @Override
+            public Object handle(Request request, Response response) {
+                String name = request.queryParams("name");
+                if (name == null || name.equals("")) {
+                    name = "Anonymous";
+                }
+
+                return String.format("Hello, %s", name);
+            }
+
+        });
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    @Override
+    public void init() {
+        setupRoutes();
+    }
+}
