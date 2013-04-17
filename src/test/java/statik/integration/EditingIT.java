175f217c3906941d1a61400c062c1087f1b87085
Rory Gibson <rory@badgersystems.co.uk>
2013-04-17 19:27:39 +0100

    Test for editing of second element in a sequence of paragraphs

--- src/test/java/statik/integration/EditingIT.java
+++ src/test/java/statik/integration/EditingIT.java
@@ -1,5 +1,6 @@
 package statik.integration;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -8,21 +9,45 @@ import static junit.framework.Assert.assertEquals;
 
 public class EditingIT extends AbstractWebDriverIntTst {
 
+
+    @Before
+    public void performLogin() {
+        doLoginWith("rory", "password");
+    }
+
     @Test
     public void simpleEdit() {
-        doLoginWith("rory", "password");
-
         driver.get(ONE_PARA_TEST_PAGE);
-        WebElement para = driver.findElement(By.cssSelector("p"));
-        assertEquals("Text not as expected", "content", para.getText());
-
-        para.click();
-        para.clear();
-        para.sendKeys("new content");
-        driver.findElement(By.tagName("body")).click();
+        changeContentOf("p", "new content");
 
         WebElement again = driver.findElement(By.cssSelector("p"));
         assertEquals("Text not as expected", "new content", again.getText());
+    }
+
+
+    @Test
+    public void editOfSecondParaInASequence() {
+        driver.get(TWO_PARA_TEST_PAGE);
+
+        WebElement atFirst = driver.findElements(By.tagName("p")).get(1);
+        assertEquals("Should be the second para", "two", atFirst.getText());
+
+        changeContentOf(atFirst, "new content");
+
+        WebElement afterEdit = driver.findElements(By.tagName("p")).get(1);
+        assertEquals("Should now hve some different content", "new content", afterEdit.getText());
+    }
+
+    private void changeContentOf(WebElement el, String newContent) {
+        el.click();
+        el.clear();
+        el.sendKeys(newContent);
+        driver.findElement(By.tagName("body")).click();
+    }
+
+    private void changeContentOf(String tag, String newContent) {
+        WebElement para = driver.findElement(By.cssSelector(tag));
+        changeContentOf(para, newContent);
     }
 
 
