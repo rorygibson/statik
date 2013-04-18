107d1b642d32485cc9631249585f81082b3ea7ba
Rory Gibson <rory@badgersystems.co.uk>
2013-04-18 09:44:21 +0100

    Qunit tests for elements of editing behaviour

--- test-website/qunit-tests.js
+++ test-website/qunit-tests.js
@@ -1,4 +1,5 @@
 
+module("DOM stuff");
 test("should focus on named field", function () {
     var $fixture = $("#qunit-fixture");
     $fixture.append("<input /> <input /> <input id=\"id-of-my-input\" name=\"my-input\" />");
@@ -8,3 +9,16 @@ test("should focus on named field", function () {
     var active = document.activeElement;
     equal(active.getAttribute("id"), "id-of-my-input", "Should have focussed on the correct input");
 });
+
+
+module("Editing");
+test("Caches original data", function() {
+    var $fixture = $("#qunit-fixture");
+    $fixture.append("<p>original content</p>");
+
+    cacheOriginalValue($('#qunit-fixture p'));
+    $('#qunit-fixture p').text('new content');
+
+    var valueInDataAttribute = $('#qunit-fixture p').data('before');
+    equal("original content", valueInDataAttribute, "Should have cached the initial value in the daa attribute");
+})
