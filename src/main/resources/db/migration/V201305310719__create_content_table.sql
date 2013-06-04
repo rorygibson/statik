ef33b248668571ba6bd6992eb9949ee9026a7531
Rory Gibson <rory@badgersystems.co.uk>
2013-06-04 16:21:26 +0100

    Tweaks to run on MySQL; decent doco in config.properties

--- src/main/resources/db/migration/V201305310719__create_content_table.sql
+++ src/main/resources/db/migration/V201305310719__create_content_table.sql
@@ -2,8 +2,8 @@ create table statik_content (
     domain varchar(255) not null,
     path varchar(255) not null,
     selector varchar(255) not null,
-    content clob,
-    is_copy boolean default false,
-    is_live boolean default false,
+    content varchar(60535),
+    is_copy boolean,
+    is_live boolean,
     language varchar(2) default 'en'
 )
